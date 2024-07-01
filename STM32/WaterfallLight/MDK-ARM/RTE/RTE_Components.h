1151234634ccb5695bffdc64570e2963a21f8ef4
ZijinJiang <2207428258@qq.com>
2024-07-01 10:50:41 +0800

    first commit

---
+++ STM32/WaterfallLight/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'J_WaterfallLight' 
+ * Target:  'J_WaterfallLight' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ requirements.md
@@ -0,0 +1,3 @@
+设计电路采用双面板，尺寸控制在10cm*10cm之内,要求手工布线，空白处写上组号，设计平台采用“嘉立创EDA”
+
+![image-20240701084342722](C:\Users\user\AppData\Roaming\Typora\typora-user-images\image-20240701084342722.png)
